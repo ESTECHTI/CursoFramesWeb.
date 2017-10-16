27ccc7f126abb25da600c2f8dbcde4c7ed301248
Elisandro <e.s.tech@outlook.com>
2017-10-16 10:29:21 -0200

    Atualizações de pacotes

--- angular1/app/dashboard/dashboardController.js
+++ angular1/app/dashboard/dashboardController.js
@@ -6,8 +6,8 @@ angular.module('primeiraApp').controller('DashboardCtrl', [
 
 function DashboardController($http) {
   const vm = this
-  vm.getSummary = function() {
-    const url = 'http://localhost:3003/api/billingSummary'
+  vm.getSummary = () => {
+    const url = 'http://localhost/3003/api/billingSummary'
     $http.get(url).then(function(response) {
       const {credit = 0, debt = 0} = response.data
       vm.credit = credit
