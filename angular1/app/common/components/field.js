b47f5e79cc53aef12874167369c4a65e47f2e07c
Elisandro <e.s.tech@outlook.com>
2017-12-28 10:21:38 -0200

    Atualizações

--- angular1/app/common/components/field.js
+++ angular1/app/common/components/field.js
@@ -6,11 +6,12 @@
       grid: '@',
       placeholder: '@',
       type: '@',
+      model: '=',
     },
     controller: [
       'gridSystem',
       function(gridSystem) {
-        this.gridClasses = gridSystem.toCssClasses(this.grid)
+        this.$onInit = () => this.gridClasses = gridSystem.toCssClasses(this.grid)
       }
     ],
     template: `
@@ -18,7 +19,7 @@
       <div class="form-group">
         <label for="{{ $ctrl.id }}">{{ $ctrl.label }}</label>
         <input id=" {{ $ctrl.id }}" class="form-control" placeholder="{{ $ctrl.placeholder }}"
-        type="{{ $ctrl.type }}" />
+        type="{{ $ctrl.type }}" ng-model="$ctrl.model" />
       </div>
     </div>
     `
